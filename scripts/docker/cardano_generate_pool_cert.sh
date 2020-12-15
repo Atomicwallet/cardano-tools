cb5564c4199621350630df4e3dd081568c59d9ca
atomlab <site-register@ya.ru>
2020-12-15 20:34:08 +0300

    Update cardano_generate_pool_cert.sh

--- scripts/docker/cardano_generate_pool_cert.sh
+++ scripts/docker/cardano_generate_pool_cert.sh
@@ -2,13 +2,17 @@
 set -e
 
 # Export env variables
-# export CARDANO_PLEDGE_POOL=100000
-# export CARDANO_COST=345000000
 # export CARDANO_PLEDGE_POOL=1000000000
-# export CARDANO_MARGIN=0.03
+# export CARDANO_COST=345000000
+# export CARDANO_MARGIN=0.05
 # export CARDANO_RALAY_HOST=cardano-relay.domain.com
-# export CARDANO_META_URL=https://domain.com
-# export CARDANO_META_HASH=e4a0039299693d0a952bedf578033ee6d76bafdd21a731e475923d40ac91d761
+# export CARDANO_METADATA_URL=https://domain.com/poolMetadata.json
+
+#!/bin/bash
+set -ex
+
+curl -o poolMetadata.json -fSL ${CARDANO_METADATA_URL}
+CARDANO_METADATA_HASH=$(docker run -it --rm -v ${PWD}:/keys --workdir /keys --entrypoint "" inputoutput/cardano-node:1.21.1 cardano-cli shelley stake-pool metadata-hash --pool-metadata-file poolMetaData.json)
 
 docker run -it --rm -v ${PWD}:/keys --workdir /keys --entrypoint "" inputoutput/cardano-node:1.23.0 \
 cardano-cli shelley stake-pool registration-certificate \
@@ -22,6 +26,6 @@ cardano-cli shelley stake-pool registration-certificate \
 --mainnet \
 --single-host-pool-relay ${CARDANO_RALAY_HOST} \
 --pool-relay-port 6001 \
---metadata-url ${CARDANO_META_URL} \
---metadata-hash ${CARDANO_META_HASH} \
+--metadata-url ${CARDANO_METADATA_URL} \
+--metadata-hash ${CARDANO_METADATA_HASH} \
 --out-file pool.cert
