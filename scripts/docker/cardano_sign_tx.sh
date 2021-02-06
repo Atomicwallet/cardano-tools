8fcb72c5530dc97a225c963a7f419bc694e17d39
atomlab <site-register@ya.ru>
2021-02-06 13:04:50 +0300

    Update cardano_sign_tx.sh

--- scripts/docker/cardano_sign_tx.sh
+++ scripts/docker/cardano_sign_tx.sh
@@ -3,7 +3,7 @@
 set -e
 
 docker run -it --rm -v ${PWD}:/keys --workdir /keys --entrypoint "" inputoutput/cardano-node:1.23.0 \
-cardano-cli shelley transaction sign \
+cardano-cli transaction sign \
 --tx-body-file tx.raw \
 --signing-key-file payment.skey \
 --signing-key-file node.skey \
