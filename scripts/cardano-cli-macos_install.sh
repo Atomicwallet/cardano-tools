62f44fbdd990c089b0c77670de8239aa8e049c83
atomlab <site-register@ya.ru>
2021-02-17 13:03:45 +0300

    Update cardano-cli-macos_install.sh

--- scripts/cardano-cli-macos_install.sh
+++ scripts/cardano-cli-macos_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export TMP_CARDANO_PATH=$(mktemp)
+export TMP_CARDANO_PATH=$(mktemp -d)
 echo $TMP_CARDANO_PATH
 curl -s https://hydra.iohk.io/build/5577872/download/1/cardano-node-1.25.0-macos.tar.gz | \
     tar -v -C ${TMP_CARDANO_PATH} -xzf - \
